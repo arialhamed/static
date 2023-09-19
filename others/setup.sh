756a6d7c2580caf50b0cbff3560a75538f1467e0
arialhamed <arifhamed.arial@outlook.com>
2023-09-19 12:06:49 +0800

    19-Sep-2023 12:06:48

--- others/setup.sh
+++ others/setup.sh
@@ -11,7 +11,7 @@ sudo snap refresh
 echo -e "\nInstalling all packages recommended by Ari"
 sudo apt install --no-install-recommends wine lm-sensors ffmpeg git gh asciinema adb openscad apksigner qbittorrent hwinfo powertop powerstat fancontrol traceroute tint quadrapassel gnome-disk-utility gdebi gedit steam vlc obs-studio krita inkscape telegram-desktop kamoso kdenlive imagemagick baobab tlp tlp-rdw apktool plasma-workspace-wayland cmake qt5-qmake flatpak audacity hw-probe blender virtualbox freecad libnotify-bin python3-pip default-jre default-jdk software-properties-common gimp meshlab nmap net-tools bible-kjv docker.io acpi pv neofetch zpaq torbrowser-launcher npm fprintd libpam-fprintd w3m w3m-img xautomation r-base-core d1x-rebirth d2x-rebirth curl ca-certificates libxext-dev clang gcc python3.10-venv nomacs heif-gdk-pixbuf heif-thumbnailer qt6-base-dev qt6-base-private-dev libqt6svg6-dev make g++ pkg-config libavcodec-dev libavformat-dev libavutil-dev libswscale-dev easytag libxi-dev libxrandr-dev libudev-dev libevdev-dev libsfml-dev libminiupnpc-dev libmbedtls-dev libcurl4-openssl-dev libhidapi-dev libsystemd-dev libbluetooth-dev libasound2-dev libpulse-dev libpugixml-dev libbz2-dev libzstd-dev liblzo2-dev libpng-dev libusb-1.0-0-dev gettext python3-pyqt5.qtsvg img2pdf gifsicle jstest-gtk supertuxkart php-common libapache2-mod-php php-cli composer dos2unix edid-decode
 
-echo -e "\nRemoving KTorrent & Gwenview (replaced with QBitTorrent & Nomacs"
+echo -e "\nRemoving KTorrent & Gwenview (replaced with QBitTorrent & Nomacs)"
 sudo apt remove ktorrent gwenview -y
 
 echo -e "\nInstalling AIMP"
@@ -22,6 +22,9 @@ wget `curl -Ls -o /dev/null -w %{url_effective} "https://discord.com/api/downloa
 
 echo -e "\nInstalling Minecraft"
 wget https://launcher.mojang.com/download/Minecraft.deb && sudo dpkg --install --force-depends Minecraft.deb && rm *.deb
+
+echo -e "\nInstalling Grabber"
+sudo flatpak install https://dl.flathub.org/repo/appstream/org.bionus.Grabber.flatpakref -y
 
 echo -e "\nInstalling pdftk, zoom, openjdk, sublime-text & android studio"
 sudo snap install pdftk zoom-client openjdk
@@ -55,12 +58,11 @@ echo "alias mv2='rsync -aP --remove-source-files'" >> ~/.bashrc
 echo "alias neofetch='neofetch --ascii_distro Kubuntu'" >> ~/.bashrc
 echo "alias python='python3'" >> ~/.bashrc
 echo "alias hw-probe='sudo -E hw-probe --all --upload'" >> ~/.bashrc
+echo "alias grabber='flatpak run org.bionus.Grabber --filename \"%search%/%date:format=yyyy-MM-dd%_%md5%.%ext%\"'" >> ~/.bashrc
 
 echo -e "\nCreating Battery Threshold script"
 sudo touch /usr/bin/batterythreshold.sh
 sudo echo "!/bin/bash" >> /usr/bin/batterythreshold.sh
-sudo echo "#echo -e password | (echo 85 | sudo -S tee /sys/class/power_supply/BAT0/charge_stop_threshold)" >> /usr/bin/batterythreshold.sh
-sudo echo "#echo -e password | (echo 75 | sudo -S tee /sys/class/power_supply/BAT0/charge_start_threshold)" >> /usr/bin/batterythreshold.sh
 sudo echo "echo 85 | tee /sys/class/power_supply/BAT0/charge_stop_threshold" >> /usr/bin/batterythreshold.sh
 sudo echo "echo 75 | tee /sys/class/power_supply/BAT0/charge_start_threshold" >> /usr/bin/batterythreshold.sh
 
